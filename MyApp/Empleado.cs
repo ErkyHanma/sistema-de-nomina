93120d08c18506bd7e55087d42789ead144eb1df
Erkyhanma <yenzel.bt@gmail.com>
2025-05-24 18:04:29 -0400

    Improve Empleado class

--- MyApp/Empleado.cs
+++ MyApp/Empleado.cs
@@ -1,10 +1,44 @@
-﻿namespace SistemaNomina.MyApp
+﻿using System;
+
+namespace SistemaNomina.MyApp
 {
     public abstract class Empleado
     {
-        public string PrimerNombre { get; protected set; }
-        public string ApellidoPaterno { get; protected set; }
-        public string NumeroSeguroSocial { get; protected set; }
+        private string primerNombre;
+        private string apellidoPaterno;
+        private string numeroSeguroSocial;
+        public string PrimerNombre
+        {
+            get => primerNombre;
+            protected set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("El nombre no puede estar vacío.");
+                primerNombre = value;
+            }
+        }
+
+        public string ApellidoPaterno
+        {
+            get => apellidoPaterno;
+            protected set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("El apellido no puede estar vacío.");
+                apellidoPaterno = value;
+            }
+        }
+
+        public string NumeroSeguroSocial
+        {
+            get => numeroSeguroSocial;
+            protected set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("El número de seguro social no puede estar vacío.");
+                numeroSeguroSocial = value;
+            }
+        }
 
         public Empleado(string primerNombre, string apellidoPaterno, string numeroSeguroSocial)
         {
@@ -13,20 +47,11 @@
             this.NumeroSeguroSocial = numeroSeguroSocial;
         }
 
-
         public override string ToString()
         {
             return $"Nombre: {PrimerNombre} {ApellidoPaterno}\nSeguro Social: {NumeroSeguroSocial}";
         }
 
         public abstract decimal CalcularPago();
-
-
-
     }
 }
-
-
-
-
-
